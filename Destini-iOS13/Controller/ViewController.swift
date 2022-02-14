fa7d9c413e91d3f749535aecb811ffd810df2f6f
brGuirra <br.guirra@gmail.com>
2022-02-14 16:18:55 -0300

    feat: add the structure to contain the stories

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,10 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet var choiceButton1: UIButton!
     @IBOutlet var choiceButton2: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger", choice1: " Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = stories[0].title
+        choiceButton1.setTitle(stories[0].choice1, for: .normal)
+        choiceButton2.setTitle(stories[0].choice2, for: .normal)
     }
 
 
